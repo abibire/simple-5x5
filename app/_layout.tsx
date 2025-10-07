e17629d9bba64c0b8d29a02e095629a270f63e86
abibire <ajbibire@gmail.com>
2025-10-06 20:54:10 -0500

    removing expo default project files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -35,10 +35,6 @@ export default function RootLayout() {
                 headerShown: false
               }}
             />
-            <Stack.Screen
-              name="modal"
-              options={{ presentation: "modal", title: "Modal" }}
-            />
           </Stack>
           <StatusBar style="light" backgroundColor="#2563eb" />
         </ThemeProvider>
